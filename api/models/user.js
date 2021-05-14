ee79cee82eb8a3a393af5abd561f1dfc97f16f1c
Vipin <demons_orca@pop-os.localdomain>
2021-05-14 19:25:08 +0530

    Added API's

---
+++ api/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    firstName: { type: String, require: true},
+    lastName: { type: String, require: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+     },
+     password: {
+         type: String,
+         required: true
+     },
+     contact: { type: String },
+     profilePic: { type: String },
+     createdAt: Date,
+     updatedAt: Date
+    
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const authenticate = require('./api/middleware/authenticate');
+
+const MONGODB_URI = 'mongodb://127.0.0.1/MyStore'
+
+mongoose.connect(MONGODB_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected',() => {
+    console.log('Mongoose is connected')
+})
+
+const userRoutes = require('./api/routes/users');
+
+
+
+
+
+
+
+app.use(express.json());
+
+
+app.use('/user', userRoutes);
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Not Found'
+    })
+})
+
+
+
+module.exports = app;
+
+
